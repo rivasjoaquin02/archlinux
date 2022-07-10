eed0b9a27d9d09cc65cd04ff6080d0f5d997996c
strange <rivasjoaquin02@gmail.com>
2022-07-09 22:17:42 -0400

    some fixes

--- base.sh
+++ base.sh
@@ -18,21 +18,24 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "strange-laptop" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo "127.0.1.1 strange-laptop.localdomain strange-laptop" >> /etc/hosts
 
 # root passwd
 echo root:password | chpasswd
 
+# initramfs
+mkinitcpio -P
+
 # install packages
 pacman -Syu
-pacman -S grub efibootmgr networkmanager network-manager-applet ntfs-3g btrfs-progs dialog wpa_supplicant mtools \
+pacman -S grub efibootmgr os-prober networkmanager network-manager-applet ntfs-3g btrfs-progs dialog wpa_supplicant mtools \
 		linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils \
 		dnsutils bluez bluez-utils cups hplip alsa-utils \
 		pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh reflector \
 		acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 \
-		openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober \
+		openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid  \
 		terminus-font dosfstools
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+# pacman -S nvidia nvidia-utils nvidia-settings
 
 # bootloader
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
@@ -41,14 +44,14 @@ grub-mkconfig -o /boot/grub/grub.cfg
 # enable services
 #systemctl enable reflector.timer
 systemctl enable NetworkManager
-systemctl enable bluetooth
-systemctl enable cups.service
-systemctl enable sshd
+#systemctl enable bluetooth
+#systemctl enable cups.service
+#systemctl enable sshd
 systemctl enable avahi-daemon
 systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
 systemctl enable fstrim.timer
 systemctl enable libvirtd
-systemctl enable firewalld
+#systemctl enable firewalld
 systemctl enable acpid
 
 # create user
--- gnome.sh
+++ gnome.sh
@@ -21,13 +21,13 @@ sudo pacman -S xorg gdm gnome gnome-extra gnome-tweaks
 
 # fonts
 sudo pacman -S --noconfirm dina-font tamsyn-font ttf-bitstream-vera ttf-croscore ttf-dejavu \
-            ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine \
-            noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro \
-            ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code \
-            ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts \
-            cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode \
-            adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk \
-            noto-fonts-emoji
+    ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine \
+    noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro \
+    ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code \
+    ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts \
+    cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode \
+    adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk \
+    noto-fonts-emoji
             
 sudo systemctl enable gdm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
